e3ce3d24fa626d24228a83b5ea5e7081d118b1ea
Ramanuj <69118312+taitaisama@users.noreply.github.com>
2021-06-29 16:11:43 +0530

    cutesy lil progress bars

--- batchmaking.py
+++ batchmaking.py
@@ -13,22 +13,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchvision.transforms import ToTensor
+import random
+from torch.optim.lr_scheduler import StepLR
 
 
 drive.mount("/content/drive")
-indi_dir = "individualDatasets"
-proc_img_dir = "processedImages"
-zip_path = "/content/drive/MyDrive/soml/NEWSolML-50.zip"
-data_path = "/content/SoML-50/data/"
-annotation_path = "/content/SoML-50/annotations.csv"
-annotate_df = None
-annotate_dict = {}
+_indi_dir = "individualDatasets"
+_proc_img_dir = "processedImages"
+_zip_path = "/content/drive/MyDrive/soml/NEWSolML-50.zip"
+_data_path = "/content/SoML-50/data/"
+_annotation_path = "/content/SoML-50/annotations.csv"
+_annotate_df = None
+_annotate_dict = {}
 
-with ZipFile(zip_path, 'r') as zip:
+#  parameters
+_batch_size = 20*3
+_test_size = 300*3
+_learning_rate = 1.0
+_gamma = 0.7
+_seed = 1
+_epochs = 15
+_print_interval = 10
+_first_iters =  5000
+
+with ZipFile(_zip_path, 'r') as zip:
   zip.extractall()
 
-value_name_map = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "add", "sub", "multi", "div"]
-value_nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+_value_name_map = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "add", "sub", "multi", "div"]
+_value_nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 class NeuralNetwork(nn.Module):
 
@@ -56,8 +69,51 @@ class NeuralNetwork(nn.Module):
     output = F.log_softmax(x, dim=1)
     return output
 
+def train(model, device, optimizer, number, possibleValues, iters):
+
+  global _batch_size
+  print("start|    training first ai    |end")
+  print("      ", end="")
+  model.train()
+  loss = None
+  for idx in range(iters):
+    data, labels = getIndivBatch(number, possibleValues)
+    data = torch.cat(data, 0)
+    data, labels = data.to(device), labels.to(device)
+    optimizer.zero_grad()
+    output = model(data)
+    loss = F.nll_loss(output, labels)
+    loss.backward()
+    optimizer.step()
+    if idx % int(iters/25) == 0:
+      print("=", end="")
+  print("\nFinal loss = ", loss.item())
+  print("done")
+
+# returns a _batch_size number of images from the _indi_dir
+# it can be limited to only get some specific values
+def getIndivBatch(number, possibleValues):
+
+  global _batch_size, _value_nums, _indi_dir, _value_name_map
+
+  images = []
+  labels = []
+  for i in range(_batch_size):
+
+    rand = random.randint(0, number - 1)
+    rand = possibleValues[rand]
+    labels.append(rand)
+    rand2 = random.randint(1, _value_nums[rand])
+    rand = _value_name_map[rand]
+    path = _indi_dir + "/" + rand + "_data/" 
+    img = getImage(path, str(rand2) + ".jpg")
+    images.append(torch.unsqueeze(ToTensor()(img), 0))
+  
+  return images, torch.tensor(labels)
+
 # takes an image and tells us the right, left, upper and lower boundries
 def getBounds(img):
+
   # reduce in size for easier searching
   reduce = cv2.resize(img, (48, 16))
   imgarr = [reduce[0:16, 0:16], reduce[0:16, 16:32], reduce[0:16, 32:48]]
@@ -81,12 +137,13 @@ def getBounds(img):
     bounds[k][3] = min(15, bounds[k][3] + 1)
     bounds[k][1] = max(0, bounds[k][1] - 1)
     bounds[k][2] = max(0, bounds[k][2] - 1)
+
   return bounds
 
 # takes an image of size x*y and makes it a square of size x*x if x > y
 def makeSquare (image, x, y):
-  sqsize = max(x, y)
 
+  sqsize = max(x, y)
   img = Image.fromarray(image)
   new_image = Image.new('L', (sqsize, sqsize), (255))
   box = (int((sqsize - x) / 2), int((sqsize - y) / 2))
@@ -97,6 +154,7 @@ def makeSquare (image, x, y):
 
 # takes an image and makes it into an array of three images of size 28x28
 def cropAndProcessImage(img):
+
   bounds = getBounds(img)
   images = []
   for i in range(3):
@@ -115,52 +173,60 @@ def cropAndProcessImage(img):
 
 # gets the image 'name' in img_dir
 def getImage(img_dir, name):
+
   image_path = img_dir + name
   img = cv2.imread(image_path, 0) # takes input in grayscale
   return img
 
-# gets processed images from proc_img_dir
+# gets processed images from _proc_img_dir
 def getProcessedImg(name):
-  global proc_img_dir
+
+  global _proc_img_dir
   data = []
   for i in range(1, 4):
-    data.append(cv2.imread(proc_img_dir + "/" + name + "_" + str(i)+ ".jpg"))
+    data.append(cv2.imread(_proc_img_dir + "/" + name + "_" + str(i)+ ".jpg"))
 
   return data
 
 def processAllImgs():
 
-  global proc_img_dir, data_path, annotate_df
-
-  for idx in annotate_df.index:
-    name = annotate_df['Image'][idx]
-    data = cropAndProcessImage(getImage(data_path, name))
+  global _proc_img_dir, _data_path, _annotate_df
+  print("start|    processing images    |end")
+  print("      ", end="")
+  for idx in _annotate_df.index:
+    name = _annotate_df['Image'][idx]
+    data = cropAndProcessImage(getImage(_data_path, name))
     name = name[0: -4]
     for i in range(3):
-      cv2.imwrite(proc_img_dir + "/" + name + "_"  + str(i+1) + ".jpg", data[i])
+      cv2.imwrite(_proc_img_dir + "/" + name + "_"  + str(i+1) + ".jpg", data[i])
+    if idx % int(len(_annotate_df.index) / 25) == 0:
+      print("=", end="")
+  print("\ndone")
 
 def initialProcessing():
 
-  global value_name_map, indi_dir, proc_img_dir, annotate_df
+  global _value_name_map, _indi_dir, _proc_img_dir, _annotate_df
 
   # first make all the directories
-  os.system("mkdir " + indi_dir)
-  os.system("mkdir " + proc_img_dir)
+  os.system("mkdir " + _indi_dir)
+  os.system("mkdir " + _proc_img_dir)
   for i in range(14):
-    os.system("mkdir " + indi_dir + "/" + value_name_map[i] + "_data")
+    os.system("mkdir " + _indi_dir + "/" + _value_name_map[i] + "_data")
 
-  annotate_df = pd.read_csv(annotation_path)
+  _annotate_df = pd.read_csv(_annotation_path)
   
   processAllImgs()
   
 def processAnnotations():
 
-  global value_name_map, annotate_df, value_nums, indi_dir, annotation_path, annotate_dict
+  global _value_name_map, _annotate_df, _value_nums, _indi_dir, _annotation_path, _annotate_dict
 
-  for idx in annotate_df.index:
-    result = annotate_df['Value'][idx]
-    fix = annotate_df['Label'][idx]
-    name = annotate_df['Image'][idx]
+  print("start|   making first batch    |end")
+  print("      ", end="")
+  for idx in _annotate_df.index:
+    result = _annotate_df['Value'][idx]
+    fix = _annotate_df['Label'][idx]
+    name = _annotate_df['Image'][idx]
     do_proc = True
     do_nums = True
     f, s, o = -1, -1, -1
@@ -187,27 +253,38 @@ def processAnnotations():
       if fix == "infix":
         first, oper, second = images[0], images[1], images[2]
         if do_nums:
-          annotate_dict[name] = [f, o, s]
+          _annotate_dict[name] = [f, o, s]
       elif fix == "prefix":
         oper, first, second = images[0], images[1], images[2]
         if do_nums:
-          annotate_dict[name] = [o, f, s]
+          _annotate_dict[name] = [o, f, s]
       else:
         first, second, oper = images[0], images[1], images[2]
         if do_nums:
-          annotate_dict[name] = [f, s, o]
+          _annotate_dict[name] = [f, s, o]
       if do_nums:
-        value_nums[f] += 1
-        cv2.imwrite(indi_dir + "/" + value_name_map[f] + "_data/" + str(value_nums[f]) + ".jpg", first)
-        value_nums[s] += 1
-        cv2.imwrite(indi_dir + "/" + value_name_map[s] + "_data/" + str(value_nums[s]) + ".jpg", second)
-      value_nums[o] += 1
-      cv2.imwrite(indi_dir + "/" + value_name_map[o] + "_data/" + str(value_nums[o]) + ".jpg", oper)
+        _value_nums[f] += 1
+        cv2.imwrite(_indi_dir + "/" + _value_name_map[f] + "_data/" + str(_value_nums[f]) + ".jpg", first)
+        _value_nums[s] += 1
+        cv2.imwrite(_indi_dir + "/" + _value_name_map[s] + "_data/" + str(_value_nums[s]) + ".jpg", second)
+      _value_nums[o] += 1
+      cv2.imwrite(_indi_dir + "/" + _value_name_map[o] + "_data/" + str(_value_nums[o]) + ".jpg", oper)
+    if idx % int(len(_annotate_df.index) / 25) == 0:
+      print("=", end="")
+  print("\ndone")
 
 def main():
 
+  global _seed, _learning_rate, _gamma, _first_iters
   initialProcessing()
   processAnnotations()
+  device = torch.device("cuda"  if torch.cuda.is_available() else "cpu")
+  torch.manual_seed(_seed)
+  np.random.seed(_seed)
+  random.seed(_seed)
+  model = NeuralNetwork().to(device)
+  optimizer = optim.Adadelta(model.parameters(), lr=_learning_rate)
+  scheduler = StepLR(optimizer, step_size=1, gamma=_gamma)
+  train(model, device, optimizer, 8, [0, 5, 7, 8, 9, 10, 11, 12], _first_iters)
 
 main()
-
